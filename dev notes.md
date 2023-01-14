37ee5e5c3494e904ce43ce9bec4448325b54d00c
Dennis Jefferson <dennismjeferson@gmail.com>
2023-01-14 10:59:55 -0800

    Created board array

--- dev notes.md
+++ dev notes.md
@@ -8,3 +8,14 @@ read the challenge pdf @ 10:37
 
 install and start node server @ 10:38
 
+start code review @ 10:41
+
+init git repo, incase I want to rollback and to further track progress @ 10:42
+
+start adding pseudo code and comments for requirements @ 10:43
+
+starting reading custom elements docs to get a better understanding of the api @ 10:44
+
+start coding board array @ 10:47
+
+review my paperchess(https://github.com/djfrsn/paperchess) code to see how I implemented the chess board array @ 10:45
--- javascript/components/board.js
+++ javascript/components/board.js
@@ -3,8 +3,39 @@ import htm from 'https://unpkg.com/htm?module'
 const html = htm.bind(jsxElem.createElement);
 
 class ChessBoard extends HTMLElement {
+  constructor() {
+    super();
+
+    // init board array
+    // the idea is to create a 2d array of objects that represent the board
+    // each object will have a position, color, and piece
+    // position = [row, column]
+    // color = 'white' or 'black'
+    // piece = null or a piece object
+
+    // create a 8x8 2d array
+    const board = []
+
+    // iterate 8 times for each row
+    for (let i = 0; i < 8; i++) {
+      // iterate 8 times for each column
+      for (let j = 0; j < 8; j++) {
+        board.push({
+          // coordinates are i = row, j = column
+          position: [i, j],
+          // even squares are white, odd squares are black
+          color: (i + j) % 2 === 0 ? 'white' : 'black',
+          piece: null
+        })
+      }
+    }
+
+    console.log(board)
+    // init pieces
+  }
   connectedCallback() {
-    render(html`<h1>Render your board here!</h1>`, this);
+    // NOTE: I noticed in the mdn example shadowRoot.append is used to add elements....could we have done that here? I'm not sure if it's necessary...just wondering
+    render(html`<h1>Render your board here!</h1>`, 4);
 
     // Example event handler
     chessEvents.on('move', ({detail}) => {
